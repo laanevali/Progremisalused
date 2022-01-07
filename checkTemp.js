2c4c3cf0674a9474d541a4ca0ecf35568876579b
Rebecca Laaneväli <95487054+laanevali@users.noreply.github.com>
2022-01-07 09:13:48 +0200

    Add files via upload

---
+++ checkTemp.js
@@ -0,0 +1,15 @@
+let sisend = prompt("Sisesta temperatuur")
+function checkTemp(a) {
+    if (a<20) {
+    console.log("liiga külm")
+    return -1;
+    } else if (a>21 && a<40) {
+    console.log("paras")
+    return 0;
+    } else {
+        console.log("liiga kuum")
+        return 1;
+    }
+}
+     
+checkTemp(sisend)
