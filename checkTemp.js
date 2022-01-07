04b8a3f2ec6db256bf1d469b0206c4feab112af5
Rebecca Laaneväli <95487054+laanevali@users.noreply.github.com>
2022-01-07 09:20:32 +0200

    Update checkTemp.js

--- checkTemp.js
+++ checkTemp.js
@@ -3,7 +3,7 @@ function checkTemp(a) {
     if (a<=20) {
     console.log("liiga külm")
     return -1;
-    } else if (a>=21 && a<40) {
+    } else if (a>=21 && a<=40) {
     console.log("paras")
     return 0;
     } else {
